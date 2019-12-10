527bf4d3f93fe71a4725e213f4a9f4894c0efd0b
Me <Me>
2019-12-10 01:44:20 -0500

    feat: Login minally working, added logo and icons to the project

--- PokedexProject/LoginViewController.swift
+++ PokedexProject/LoginViewController.swift
@@ -14,10 +14,44 @@ class LoginViewController: UIViewController {
     
     @IBOutlet private weak var tfPassword: UITextField!
     
+    @IBOutlet weak var lblErrorMsg: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let user = UserModel()
+        user.email = "x@y.com"
+        user.password = "jjj"
+        CoreDataSaveOps.shared.saveUser(userObject: user)
     }
     
+    
+    @IBAction func btnActionLogin(_ sender: Any) {
+        
+        guard let email = tfEmail.text else { return }
+        guard let password = tfPassword.text else { return }
+        
+        if !isValidEmail(emailStr: email) {
+            lblErrorMsg.text = "Email format invalid. Please retype."
+            return
+        }
+        
+        let users = CoreDataFetchOps.shared.getUserby(email: email)
+        if users.count != 0 {
+            if users[0].password == password {
+                print("Logged in")
+            } else {
+                lblErrorMsg.text = "Invalid Password, Please try again."
+            }
+        }
+    }
+    
+    func isValidEmail(emailStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: emailStr)
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.

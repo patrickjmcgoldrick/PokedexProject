a75aab06224925e0088c68a70ecb19737ffe4d2a
Me <Me>
2019-12-11 03:39:09 -0500

    feat: DetailView Segment control is now working

--- PokedexProject/DetailViewController.swift
+++ PokedexProject/DetailViewController.swift
@@ -8,15 +8,38 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UISearchBarDelegate {
 
     var pokemon: Pokemon?
+    
+    
+    @IBOutlet weak var detailPane: UIView!
+    
+    @IBOutlet weak var evolutionPane: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         if let name = pokemon?.name {
             navigationController?.navigationBar.topItem?.title = name
+        }
+    
+    }
+
+    @IBAction func segmentControl(_ sender: UISegmentedControl) {
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print ("detail")
+           
+            view.sendSubviewToBack(evolutionPane)
+
+        case 1:
+            print ("evolution")
+            view.sendSubviewToBack(detailPane)
+
+        default:
+            break
         }
     }
 }
